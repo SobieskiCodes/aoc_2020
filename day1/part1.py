babfa0a8bffee4c58184ba32a48c049d16801f7d
Justin Sobieski <justin@sobieski.codes>
2020-12-01 09:23:01 -0500

    Update part1.py

--- day1/part1.py
+++ day1/part1.py
@@ -1,4 +1,10 @@
 with open('day1/input.txt', 'r') as f:
     r = [int(i.strip()) for i in f.readlines()]
 answer = [i * r[l] for i in r for l in range(0, len(r)) if i + r[l] == 2020][0]
+# an = 0
+# for i in r:
+#     for l in range(0, len(r)):
+#         if i + r[l] == 2020:
+#             an = i * r[l]
+# print(an)
 print(answer)
