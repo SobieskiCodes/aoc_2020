7de2cd7310d5d84e92057ad1b556757e572a78cf
Justin Sobieski <justin@sobieski.codes>
2020-12-01 10:32:57 -0500

    faster faster, drive faster dad!

--- day1/day1.py
+++ day1/day1.py
@@ -1,7 +1,9 @@
 from itertools import combinations
 import operator
 import functools
+import time
+start = time.time()
 with open('day1/input.txt', 'r') as f:
     r = [int(i.strip()) for i in f.readlines()]
-print(f"p1: {[i * r[l] for i in r for l in range(0, len(r)) if i + r[l] == 2020][0]}")
-print(f"p2: {functools.reduce(operator.mul, list([c for c in combinations(r, 3) if sum(c) == 2020][0]), 1)}")
+print(f"p1: {next(i * r[l] for i in r for l in range(0, len(r)) if i + r[l] == 2020)} time: {time.time() - start}")
+print(f"p2: {functools.reduce(operator.mul, next(c for c in combinations(r, 3) if sum(c) == 2020), 1)} time: {time.time() - start}")
