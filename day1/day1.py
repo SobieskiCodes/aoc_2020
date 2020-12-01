7bc2508de374bf89ca5c8f4160a463b859b47822
Justin Sobieski <justin@sobieski.codes>
2020-12-01 15:59:22 -0500

    Update day1.py

--- day1/day1.py
+++ day1/day1.py
@@ -1,9 +1,8 @@
 from itertools import combinations
-from operator import mul
-from functools import reduce
+from math import prod
 import time
 start = time.time()
 with open('day1/input.txt', 'r') as f:
     r = [int(i.strip()) for i in f.readlines()]
 print(f"p1: {next(i * r[l] for i in r for l in range(0, len(r)) if i + r[l] == 2020)} time: {time.time() - start}")
-print(f"p2: {reduce(mul, next(c for c in combinations(r, 3) if sum(c) == 2020), 1)} time: {time.time() - start}")
+print(f"p2: {prod(next(i for i in combinations(r, 3) if sum(i) == 2020), start=1)} time: {time.time() - start}")
