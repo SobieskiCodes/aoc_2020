012fe138b69ac254f49c5d4ee04ba8c2f243ea23
Justin Sobieski <justin@sobieski.codes>
2020-12-02 08:17:20 -0500

    Update day2.py

--- day2/day2.py
+++ day2/day2.py
@@ -9,8 +9,7 @@ for i in r:
     min_v, max_v = int(matches.group(1)), int(matches.group(2))
     if min_v <= matches.group(4).count(matches.group(3)) <= max_v:
         valid += 1
-    a = matches.group(4)[min_v - 1]
-    b = matches.group(4)[max_v - 1]
+    a, b = matches.group(4)[min_v - 1], matches.group(4)[max_v - 1]
     if (a == matches.group(3) and b != matches.group(3)) or (a != matches.group(3) and b == matches.group(3)):
         p2_valid += 1
 
