8ecdf4636f8595d3391fd26dc23fa71f5ac6a32c
Justin Sobieski <justin@sobieski.codes>
2020-12-02 10:18:34 -0500

    Update day2.py

--- day2/day2.py
+++ day2/day2.py
@@ -1,6 +1,6 @@
 import re
 r = [i.strip() for i in open('./input.txt', 'r').readlines()]
-matches = [re.search("^([0-9]+)-([0-9]+) ([a-z])(?::) ([a-z]+)", i, re.IGNORECASE) for i in r]
+matches = [re.search("^([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)", i, re.IGNORECASE) for i in r]  # (\d+)-(\d+) (.): (\w+)
 p1_valid = f"p1: {len([i for i in matches if int(i.group(1)) <= i.group(4).count(i.group(3)) <= int(i.group(2))])}"
 p2_valid = f"p2: {len([i for i in matches if (i.group(4)[int(i.group(1)) - 1] == i.group(3) and i.group(4)[int(i.group(2)) - 1] != i.group(3)) or (i.group(4)[int(i.group(1)) - 1] != i.group(3) and i.group(4)[int(i.group(2)) - 1] == i.group(3))])}"
 print(f"{p1_valid}\n{p2_valid}")
