01efeea45911531a0bca5231a8d4a246dd3f6d9f
Justin Sobieski <justin@sobieski.codes>
2020-12-02 08:45:16 -0500

    Update day2.py
    who needs readability

--- day2/day2.py
+++ day2/day2.py
@@ -2,16 +2,8 @@ import re
 with open('./input.txt', 'r') as f:
     r = [i.strip() for i in f.readlines()]
 
-valid = 0
-p2_valid = 0
-for i in r:
-    matches = re.search("^([0-9]+)-([0-9]+) ([a-z])(?::) ([a-z]+)", i, re.IGNORECASE)
-    min_v, max_v = int(matches.group(1)), int(matches.group(2))
-    if min_v <= matches.group(4).count(matches.group(3)) <= max_v:
-        valid += 1
-    a, b = matches.group(4)[min_v - 1], matches.group(4)[max_v - 1]
-    if (a == matches.group(3) and b != matches.group(3)) or (a != matches.group(3) and b == matches.group(3)):
-        p2_valid += 1
-
-print(f"p1: {valid}")
-print(f"p2: {p2_valid}")
+matches = [re.search("^([0-9]+)-([0-9]+) ([a-z])(?::) ([a-z]+)", i, re.IGNORECASE) for i in r]
+p1_valid = f"p1: {len([i for i in matches if int(i.group(1)) <= i.group(4).count(i.group(3)) <= int(i.group(2))])}"
+p2_valid = f"p2: {len([i for i in matches if (i.group(4)[int(i.group(1)) - 1] == i.group(3) and i.group(4)[int(i.group(2)) - 1] != i.group(3)) or (i.group(4)[int(i.group(1)) - 1] != i.group(3) and i.group(4)[int(i.group(2)) - 1] == i.group(3))])}"
+print(p1_valid)
+print(p2_valid)
